9d7048db3699ba8a5cb99f35d930cb17441362a1
Rustem Shaydullin <ndiezel0@gmail.com>
2019-07-15 13:45:22 +0300

    CDS-92: Upgrade cds_proto

--- apps/kds/src/kds_keyring_management_thrift_handler.erl
+++ apps/kds/src/kds_keyring_management_thrift_handler.erl
@@ -6,7 +6,7 @@
 %% woody_server_thrift_handler callbacks
 -export([handle_function/4]).
 
--type encrypted_masterkey_share() :: #'EncryptedMasterKeyShare' {}.
+-type encrypted_masterkey_share() :: #'cds_EncryptedMasterKeyShare' {}.
 
 %%
 %% woody_server_thrift_handler callbacks
@@ -27,11 +27,11 @@ handle_function_('StartInit', [Threshold], _Context, _Opts) ->
             {ok, encode_encrypted_shares(EncryptedMasterKeyShares)}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         invalid_args ->
-            raise(#'InvalidArguments'{})
+            raise(#'cds_InvalidArguments'{})
     end;
 handle_function_('ValidateInit', [SignedShare], _Context, _Opts) ->
     {ShareholderId, Share} = decode_signed_share(SignedShare),
@@ -40,33 +40,33 @@ handle_function_('ValidateInit', [SignedShare], _Context, _Opts) ->
         {more, More} ->
             {ok, {more_keys_needed, More}};
         ok ->
-            {ok, {success, #'Success'{}}}
+            {ok, {success, #'cds_Success'{}}}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         {operation_aborted, Reason} ->
-            raise(#'OperationAborted'{reason = atom_to_binary(Reason, utf8)})
+            raise(#'cds_OperationAborted'{reason = atom_to_binary(Reason, utf8)})
     end;
 handle_function_('CancelInit', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:cancel_init()} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status})
+            raise(#'cds_InvalidStatus'{status = Status})
     end;
 handle_function_('Lock', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:lock()} catch
         {invalid_status, locked} ->
             {ok, ok};
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status})
+            raise(#'cds_InvalidStatus'{status = Status})
     end;
 handle_function_('StartUnlock', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:start_unlock()} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity})
+            raise(#'cds_InvalidActivity'{activity = Activity})
     end;
 handle_function_('ConfirmUnlock', [SignedShare], _Context, _Opts) ->
     {ShareholderId, Share} = decode_signed_share(SignedShare),
@@ -75,26 +75,26 @@ handle_function_('ConfirmUnlock', [SignedShare], _Context, _Opts) ->
         {more, More} ->
             {ok, {more_keys_needed, More}};
         ok ->
-            {ok, {success, #'Success'{}}}
+            {ok, {success, #'cds_Success'{}}}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         {operation_aborted, Reason} ->
-            raise(#'OperationAborted'{reason = atom_to_binary(Reason, utf8)})
+            raise(#'cds_OperationAborted'{reason = atom_to_binary(Reason, utf8)})
     end;
 handle_function_('CancelUnlock', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:cancel_unlock()} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status})
+            raise(#'cds_InvalidStatus'{status = Status})
     end;
 handle_function_('StartRotate', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:start_rotate()} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity})
+            raise(#'cds_InvalidActivity'{activity = Activity})
     end;
 handle_function_('ConfirmRotate', [SignedShare], _Context, _Opts) ->
     {ShareholderId, Share} = decode_signed_share(SignedShare),
@@ -103,28 +103,28 @@ handle_function_('ConfirmRotate', [SignedShare], _Context, _Opts) ->
         {more, More} ->
             {ok, {more_keys_needed, More}};
         ok ->
-            {ok, {success, #'Success'{}}}
+            {ok, {success, #'cds_Success'{}}}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         {operation_aborted, Reason} ->
-            raise(#'OperationAborted'{reason = atom_to_binary(Reason, utf8)})
+            raise(#'cds_OperationAborted'{reason = atom_to_binary(Reason, utf8)})
     end;
 handle_function_('CancelRotate', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:cancel_rotate()} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status})
+            raise(#'cds_InvalidStatus'{status = Status})
     end;
 handle_function_('StartRekey', [Threshold], _Context, _Opts) ->
     try {ok, kds_keyring_manager:start_rekey(Threshold)} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         invalid_args ->
-            raise(#'InvalidArguments'{})
+            raise(#'cds_InvalidArguments'{})
     end;
 handle_function_('ConfirmRekey', [SignedShare], _Context, _Opts) ->
     {ShareholderId, Share} = decode_signed_share(SignedShare),
@@ -133,14 +133,14 @@ handle_function_('ConfirmRekey', [SignedShare], _Context, _Opts) ->
         {more, More} ->
             {ok, {more_keys_needed, More}};
         ok ->
-            {ok, {success, #'Success'{}}}
+            {ok, {success, #'cds_Success'{}}}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         {operation_aborted, Reason} ->
-            raise(#'OperationAborted'{reason = atom_to_binary(Reason, utf8)})
+            raise(#'cds_OperationAborted'{reason = atom_to_binary(Reason, utf8)})
     end;
 handle_function_('StartRekeyValidation', [], _Context, _Opts) ->
     try kds_keyring_manager:start_validate_rekey() of
@@ -148,9 +148,9 @@ handle_function_('StartRekeyValidation', [], _Context, _Opts) ->
             {ok, encode_encrypted_shares(EncryptedMasterKeyShares)}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity})
+            raise(#'cds_InvalidActivity'{activity = Activity})
     end;
 handle_function_('ValidateRekey', [SignedShare], _Context, _Opts) ->
     {ShareholderId, Share} = decode_signed_share(SignedShare),
@@ -159,19 +159,19 @@ handle_function_('ValidateRekey', [SignedShare], _Context, _Opts) ->
         {more, More} ->
             {ok, {more_keys_needed, More}};
         ok ->
-            {ok, {success, #'Success'{}}}
+            {ok, {success, #'cds_Success'{}}}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {invalid_activity, Activity} ->
-            raise(#'InvalidActivity'{activity = Activity});
+            raise(#'cds_InvalidActivity'{activity = Activity});
         {operation_aborted, Reason} ->
-            raise(#'OperationAborted'{reason = atom_to_binary(Reason, utf8)})
+            raise(#'cds_OperationAborted'{reason = atom_to_binary(Reason, utf8)})
     end;
 handle_function_('CancelRekey', [], _Context, _Opts) ->
     try {ok, kds_keyring_manager:cancel_rekey()} catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status})
+            raise(#'cds_InvalidStatus'{status = Status})
     end;
 
 handle_function_('GetState', [], _Context, _Opts) ->
@@ -189,9 +189,9 @@ handle_function_('UpdateKeyringMeta', [KeyringMeta], _Context, _Opts) ->
             {ok, ok}
     catch
         {invalid_status, Status} ->
-            raise(#'InvalidStatus'{status = Status});
+            raise(#'cds_InvalidStatus'{status = Status});
         {validation_failed, Reason} ->
-            raise(#'InvalidKeyringMeta'{reason = erlang:atom_to_binary(Reason, utf8)})
+            raise(#'cds_InvalidKeyringMeta'{reason = erlang:atom_to_binary(Reason, utf8)})
     end;
 handle_function_('GetKeyringMeta', [], _Context, _Opts) ->
     KeyringMeta = kds_keyring_manager:get_meta(),
@@ -212,7 +212,7 @@ encode_encrypted_share(#{
     owner := Owner,
     encrypted_share := EncryptedShare
 }) ->
-    #'EncryptedMasterKeyShare' {
+    #'cds_EncryptedMasterKeyShare' {
         id = Id,
         owner = Owner,
         encrypted_share = EncryptedShare
@@ -232,12 +232,12 @@ verify_signed_share(ShareholderId, SignedShare, OperationId) ->
                 {error, failed_to_verify} ->
                     _ = logger:info("Shareholder ~w failed verification of operation ~w",
                         [ShareholderId, OperationId]),
-                    raise(#'VerificationFailed'{})
+                    raise(#'cds_VerificationFailed'{})
             end;
         {error, not_found} ->
             _ = logger:info("Shareholder ~w failed verification of operation ~w",
                 [ShareholderId, OperationId]),
-            raise(#'VerificationFailed'{})
+            raise(#'cds_VerificationFailed'{})
     end.
 
 encode_state(#{
@@ -266,25 +266,25 @@ encode_state(#{
         }
     }
 }) ->
-    #'KeyringState'{
+    #'cds_KeyringState'{
         status = Status,
-        activities = #'ActivitiesState'{
-            initialization = #'InitializationState'{
+        activities = #'cds_ActivitiesState'{
+            initialization = #'cds_InitializationState'{
                 phase = InitPhase,
                 lifetime = InitLifetime,
                 validation_shares = InitValShares
             },
-            rotation = #'RotationState'{
+            rotation = #'cds_RotationState'{
                 phase = RotatePhase,
                 lifetime = RotateLifetime,
                 confirmation_shares = RotateConShares
             },
-            unlock = #'UnlockState'{
+            unlock = #'cds_UnlockState'{
                 phase = UnlockPhase,
                 lifetime = UnlockLifetime,
                 confirmation_shares = UnlockConShares
             },
-            rekeying = #'RekeyingState'{
+            rekeying = #'cds_RekeyingState'{
                 phase = RekeyPhase,
                 lifetime = RekeyLifetime,
                 confirmation_shares = RekeyConShares,
@@ -293,7 +293,7 @@ encode_state(#{
         }
     }.
 
-decode_signed_share(#'SignedMasterKeyShare'{
+decode_signed_share(#'cds_SignedMasterKeyShare'{
     id = ShareholderId,
     signed_share = Share
 }) ->
